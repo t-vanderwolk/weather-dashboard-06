75dcd1ebcfb08cbd1a3616a33669f38328df1d04
t-vanderwolk <97120854+t-vanderwolk@users.noreply.github.com>
2022-03-26 10:57:57 -0700

    initail commit;thought process in read me; linked bootstrap and css and js to html files;

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+function fetch(apiURL)
+let apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.49&lon=111.926109&exclude=hourly,daily&appid=44e2b3699341d2a3a6d507833c150d02"
+.then(function (response) {
+          if (response.ok) {
+            response.json().then(function (data) {
+              displayIssues(data)
+            }
+            )
+        }
+};
